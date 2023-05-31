87a24fe87910b191c9c1eaf33ca54c7151f9a692
Jenisse Reidel <jeyula.lp@gmail.com>
2023-05-30 23:19:23 -0300

    arregle problema de cupos y error al finalizar. Funciona ok.

--- app.js
+++ app.js
@@ -1,43 +1,50 @@
 alert("¡Bienvenida a Renatta Nails Spa!");
 
 let comando;
+let cupos = 1;
 
 while (comando != "Finalizar") {
     comando = prompt('Ingresá la opción deseada:\n\n· Ver servicios\n· Solicitar turno\n· Contactanos\n· Finalizar');
     switch (comando) {
-        case 'Ver servicios': 
-            for (let i = 1; i <= 5; i++) { 
-            alert('Servicio nº ' + i);
+        case 'Ver servicios':
+            for (let i = 1; i <= 5; i++) {
+                alert('Servicio nº ' + i);
             }
             break;
         case 'Solicitar turno':
-            let nombre = prompt("Ingresa tu nombre");
-            let dia = prompt("Ingresá el día:\n· Martes\n· Jueves\n· Sabado");
-            switch (dia) {
-                case "Martes": 
-                    alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
-                    break;
-                case "Jueves": 
-                    alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
-                    break;
-                case "Sabado": 
-                    alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
-                    break;
-                default:
-                    alert("El día ingresado es incorrecto. Por favor volvé a ingresar una opción válida.")
-                    break;
+            if (cupos <= 5) {
+                let nombre = prompt("Ingresa tu nombre");
+                let dia = prompt("Ingresá el día:\n· Martes\n· Jueves\n· Sabado");
+                switch (dia) {
+                    case "Martes":
+                        alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
+                        break;
+                    case "Jueves":
+                        alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
+                        break;
+                    case "Sabado":
+                        alert(nombre + " tu turno quedó programado para el día " + dia + " y es el nº " + cupos);
+                        break;
+                    default:
+                        alert("El día ingresado es incorrecto. Por favor, intentalo nuevamente.")
+                        break;
+                }
+                cupos = cupos + 1;
+            } else {
+                alert("Lo sentimos, ya no hay turnos disponibles.")
             }
-        break;
+            break;
         case 'Contactanos':
             let nombre2 = prompt("Ingresa tu nombre");
             let mail = prompt("Ingresa tu mail de contacto");
             prompt("Dejanos tu consulta");
             alert("Gracias por escribirnos " + nombre2 + ". En breve te responderemos a " + mail);
             break;
+        case 'Finalizar':
+            comando = "Finalizar";
+            break;
+        default:
+            alert("La opción ingresada es incorrecta. Por favor, intentalo nuevamente.");
+            break;
     }
-    if (comando != "Ver Servicios" || "Solicitar turno" || "contactanos") {
-        alert('La opción ingresada es incorrecta. Por favor, volvé a intentarlo');
-    } else if (comando = "Finalizar"){
-        comando = "Finalizar";      
-    }      
 }
